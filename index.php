b31af95773bc1401b9dec7a33d01ce4c06789fc4
jonnow <jonnowitts@gmail.com>
2026-09-15 10:35:37 +0100

    Home hero and feed layout

--- index.php
+++ index.php
@@ -8,39 +8,41 @@
         </section>
         <section class="feed">
             <h2>Hot off the press!</h2>
-            <article data-type="photo">
-                <label class="type">Photo</label>
-                <img alt="alt tag for the image" />
-            </article>
-            <article data-type="work">
-                <label class="type">Work</label>
-                <h1>Project title</h1>
-                <img alt="alt tag for the image" />
-            </article>
-            <article data-type="photo">
-                <label class="type">Photo</label>
-                <img alt="alt tag for the image" />
-            </article>
-            <article data-type="work">
-                <label class="type">Work</label>
-                <h1>Project title</h1>
-                <img alt="alt tag for the image" />
-            </article>
-            <article data-type="post">
-                <label class="type">Notes</label>
-                <h1>Blog post title</h1>
-                <datetime>21 August 2026</datetime>
-                <p class="excerpt">The introductory paragraph, a.k.a “snippet” to an interesting blog post. Long enough to garner interest, short enough to fit in here.</p>
-                <ul class="tag-selection">
-                    <li><a href="#">Thing 1</a></li>
-                    <li><a href="#">Thing 2</a></li>
-                    <li><a href="#">Thing 3</a></li>
-                </ul>
-            </article>
-            <article data-type="photo">
-                <label class="type">Photo</label>
-                <img alt="alt tag for the image" />
-            </article>
+            <div class="the-loop">
+                <article data-type="photo">
+                    <label class="type">Photo</label>
+                    <img alt="alt tag for the image" />
+                </article>
+                <article data-type="work">
+                    <label class="type">Work</label>
+                    <h1>Project title</h1>
+                    <img alt="alt tag for the image" />
+                </article>
+                <article data-type="photo">
+                    <label class="type">Photo</label>
+                    <img alt="alt tag for the image" />
+                </article>
+                <article data-type="work">
+                    <label class="type">Work</label>
+                    <h1>Project title</h1>
+                    <img alt="alt tag for the image" />
+                </article>
+                <article data-type="post">
+                    <label class="type">Notes</label>
+                    <h1>Blog post title</h1>
+                    <time datetime="2026-08-21">21 August 2026</time>
+                    <p class="excerpt">The introductory paragraph, a.k.a “snippet” to an interesting blog post. Long enough to garner interest, short enough to fit in here.</p>
+                    <ul class="tag-selection">
+                        <li><a href="#">Thing 1</a></li>
+                        <li><a href="#">Thing 2</a></li>
+                        <li><a href="#">Thing 3</a></li>
+                    </ul>
+                </article>
+                <article data-type="photo">
+                    <label class="type">Photo</label>
+                    <img alt="alt tag for the image" />
+                </article>
+            </div>
             <a href="blog">Visit the feed</a>
         </section>
     </main>
